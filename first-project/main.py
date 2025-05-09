c660e56d2583173997e38be9421ae83da7f28902
Beatris <beatrisraj7@gmail.com>
2025-05-09 21:27:51 +0300

    Calculator buttons

--- first-project/main.py
+++ first-project/main.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QWidget, QPushButton
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QGridLayout, QPushButton, QWidget
 from PyQt5.QtGui import QIcon
 
 icon_path = 'C:/Users/Betty/Documents/Py_Projects/images/note_icon.png'
@@ -12,36 +12,57 @@ class MainWIndow(QMainWindow):
         self.setWindowIcon(QIcon(icon_path))
         self.initUI()
 
+        self.input = '';
+
     def initUI(self):
+        self.central_widget = QWidget(self)  # ✅ fixed typo
+        self.setCentralWidget(self.central_widget)
         self.initButtons()
-        # cental_widget = QWidget()
-        # self.setCentralWidget(cental_widget)
 
 
     def initButtons(self):
         buttons = [
-            { 'name': 'C', 'fun': self.on_click }
+            { 'name': 'C', 'fun': self.on_click },
+            { 'name': '7', 'fun': lambda: self.add_input('7')},
+            { 'name': '8', 'fun': lambda: self.add_input('8') },
+            { 'name': '9', 'fun': lambda: self.add_input('9') },
+            { 'name': '÷', 'fun': lambda: self.add_input('/') },
+            { 'name': '4', 'fun': lambda: self.add_input('4') },
+            { 'name': '5', 'fun': lambda: self.add_input('5') },
+            { 'name': '6', 'fun': lambda: self.add_input('6') },
+            { 'name': '×', 'fun': lambda: self.add_input('*') },
+            { 'name': '1', 'fun': lambda: self.add_input('1') },
+            { 'name': '2', 'fun': lambda: self.add_input('2') },
+            { 'name': '3', 'fun': lambda: self.add_input('3') },
+            { 'name': '×', 'fun': lambda: self.add_input('+') },
+            { 'name': '×', 'fun': lambda: self.add_input('0') },
+            { 'name': '×', 'fun': lambda: self.add_input('-') },
+
         ]
 
-        def createButton(btn):
-            button = QPushButton(btn['name'], self)
-            button.clicked.connect(self.on_click)
-        
-        for btn in buttons:
-            createButton(btn)
+        grid = QGridLayout()
+        self.central_widget.setLayout(grid)  
 
-        return
+        for index, btn in enumerate(buttons):
+            row = index // 4 
+            col = index % 4
+            button = QPushButton(btn['name'], self.central_widget)
+            button.clicked.connect(btn['fun'])
+            grid.addWidget(button, row, col)
+
 
     def on_click(self):
         print('i was clicked')
+    def add_input(self, input):
+        self.input += input
+
+
 
 
 
 def main():
     app = QApplication([])
     window = MainWIndow()
-    label = QLabel('Hello, PyQt!', parent=window)
-    label.move(100, 80)
     window.show()
     app.exec_()
 
