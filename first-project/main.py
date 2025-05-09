a076955033e812e75ce011907295ddb311d2d43b
Beatris <beatrisraj7@gmail.com>
2025-05-09 19:58:07 +0300

    Use Class for main window

--- first-project/main.py
+++ first-project/main.py
@@ -1,18 +1,23 @@
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
 from PyQt5.QtGui import QIcon
 
-app = QApplication([])
-window = QWidget()
-window.setWindowTitle('To do')
-window.setGeometry(100, 100, 600, 500)
-
 icon_path = 'C:/Users/Betty/Documents/Py_Projects/images/note_icon.png'
-window.setWindowIcon(QIcon(icon_path))
 
-label = QLabel('Hello, PyQt!', parent=window)
-label.move(100, 80)
+
+class MainWIndow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('To do')
+        self.setGeometry(100, 100, 600, 500)
+        self.setWindowIcon(QIcon(icon_path))
+
+
 
 def main():
+    app = QApplication([])
+    window = MainWIndow()
+    label = QLabel('Hello, PyQt!', parent=window)
+    label.move(100, 80)
     window.show()
     app.exec_()
 
