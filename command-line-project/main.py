8e6e7f278e4c8c759fd67045debba8d7801898c6
Beatris <beatrisraj7@gmail.com>
2025-05-19 18:25:08 +0300

    Create and read file

--- command-line-project/main.py
+++ command-line-project/main.py
@@ -1,10 +1,24 @@
 import sys
 
-def get_sum(num1, num2):
-    return(int(num1) + int(num2))
+def create_new_line(newLine: str):
+    with open('demo.txt', 'a') as text_file:
+        text_file.write(newLine)
+
+def read_file():
+    with open('demo.txt') as text_file:
+        print(text_file.read())
+
+def check_args():
+    if len(sys.argv) > 1:
+        return True
+    return False
+
 
 if __name__ == '__main__':
-    num1 = sys.argv[1]
-    num2 = sys.argv[2]
-    print('Res', get_sum(num1, num2))
-    print(f'arg1 ${sys.argv[0]}')
+    if check_args():
+        match(sys.argv[1]):
+            case 'c':
+                if sys.argv[2]: 
+                    create_new_line(sys.argv[2])
+            case 'r':
+                read_file()
