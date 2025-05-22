42ed64a49b5b32d394f71b4404a4aa310d8820c1
Beatris <beatrisraj7@gmail.com>
2025-05-22 19:52:38 +0300

    Update main.py

--- command-line-project/main.py
+++ command-line-project/main.py
@@ -1,12 +1,24 @@
 import sys
 
-def create_new_line(newLine: str):
+
+def create_new_line():
+    if len(sys.argv) < 3 or not sys.argv[2]:
+        print('No new line provided')
+        return
+
     with open('demo.txt', 'a') as text_file:
-        text_file.write(newLine)
+        text_file.write(sys.argv[2])
 
 def read_file():
     with open('demo.txt') as text_file:
         print(text_file.read())
+
+
+commands = {
+    'c': {'func':create_new_line, 'desc': 'Creates a new line in demo.txt' },
+    'r':{'func':read_file, "desc":'Reads demo.txt contnent'},
+
+}
 
 def check_args():
     if len(sys.argv) > 1:
@@ -15,10 +27,5 @@ def check_args():
 
 
 if __name__ == '__main__':
-    if check_args():
-        match(sys.argv[1]):
-            case 'c':
-                if sys.argv[2]: 
-                    create_new_line(sys.argv[2])
-            case 'r':
-                read_file()
+    if check_args() and sys.argv[1] in commands:
+        commands[sys.argv[1]]['func']()
